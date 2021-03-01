ea740caeb42652f6717e2cae6932562b92cb3010
muzakidian <muzakidian1@gmail.com>
2021-03-01 22:03:45 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//1. Halaman Home (routes biasa)
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+//2. Halaman produk (routes prefix)
+Route::prefix('/product')->group(function(){
+    Route::get('/product', [ProductController::class, 'index'])->name('product');
+});
+
+//3. Halaman News (routes parameter)
+Route::get('/news/{title}', [NewsController::class, 'index'])->name('news');
+
+//4. Halaman About-us (routes biasa)
+Route::get('/about-us', [AboutController::class, 'index'])->name('about');
+
+//5. Halaman contact-us (routes resource only)
+Route::resource('/contact-us', ContactController::class,['only' => ['index'] ]);
